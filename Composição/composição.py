d0228b3775c4519e7998bf566fa40ab7ab098403
Julia-Marcal <anajulia.05marcal@gmail.com>
2022-09-20 17:24:45 -0300

    Composition add

--- Composição/composição.py
+++ Composição/composição.py
@@ -1,22 +1,42 @@
-from os import system
-
-class Computer:
-    def __init__(self) -> None:
-        self.bd_list = []
-
-
-    def insert_on_bd(self, system, year):
-        self.bd_list.append(Info_computer(system, year))
-        for i in self.bd_list:
-            print(i.system, i.year)
-    
-
-
-class Info_computer:
-    def __init__(self, system:str , year: int) -> None:
+class Db_computer:
+    def __init__(self,system: str, year) -> None:
+        self.db_list = []
         self.system = system
         self.year = year
 
+    def insert_on_bd(self):
+        self.db_list.append(self.system)
+        self.db_list.append(self.year)
+        for i in self.db_list:
+            print(i)
 
-c1 = Computer()
-c1.insert_on_bd('apple', 2008)
+
+class Computer:
+    def __init__(self, system:str , year: int) -> None:
+        self.system_computer = system
+        self.year_computer = year
+        self.price = None
+    
+    #getter para mandar as informações para o banco de dados 
+    @property
+    def return_system(self):
+        return self.system_computer
+        
+    #getter para mandar as informações para o banco de dados 
+    @property
+    def return_year(self):
+        return self.year_computer
+
+    def price_c(self):
+        if self.year_computer > 2000 and self.year_computer < 2010:
+            self.price = 2000
+            print('The price of this pc is: 2000')
+        else:
+            self.price = 3000
+            print('The price of this pc is: 3000')
+
+
+c1 = Computer('apple', 2008)
+c1.price_c()
+bd_br = Db_computer(c1.return_system, c1.return_year)
+bd_br.insert_on_bd()
