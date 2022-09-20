7ae7446ed3cec681c76170d75d00a14664295516
Julia-Marcal <anajulia.05marcal@gmail.com>
2022-09-20 18:06:10 -0300

    Create Herança.py

---
+++ Herança/Herança.py
@@ -0,0 +1,27 @@
+class Vehicle:
+
+    def __init__(self, name:str, year:int) ->None:
+        self.name = name 
+        self.year = year
+        self.classname = self.__class__.__name__
+
+    def motor(self) -> None:
+        print(f"Vroom vroom I'm a {self.classname}")
+
+
+class Bicycle(Vehicle):
+    def __init__(self, name, year):
+        super().__init__(name, year)
+
+    def DocumentationToPay(self):
+        print(f'As your vehicle is a {self.classname} you have to pay 1100$ for your documentation')
+
+
+class Car(Vehicle):
+    def DocumentationToPay(self):
+        print(f'As your vehicle is a {self.classname} you have to pay 3000$ for your documentation')
+
+
+Carro = Car('Mustang', 2010)
+Carro.DocumentationToPay()
+Carro.motor()
